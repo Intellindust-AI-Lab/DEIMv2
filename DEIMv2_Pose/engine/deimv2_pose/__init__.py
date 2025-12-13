f203a286b3b59782df036b028d0c89d6a33e2dca
QiruiWang0728 <1419663087@qq.com>
2025-12-13 14:20:55 +0800

    delete DEIMv2_Pose

--- DEIMv2_Pose/engine/deimv2_pose/__init__.py
+++
@@ -1,6 +0,0 @@
-from .deimv2_pose import DEIMv2Pose
-from .hybrid_encoder import HybridEncoder
-from .criterion import Criterion
-from .postprocesses import PostProcessor
-from .transformer import Transformer
-from .matcher import HungarianMatcher
